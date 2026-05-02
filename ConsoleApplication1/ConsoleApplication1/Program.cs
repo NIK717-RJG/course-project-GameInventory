f0c5479d2dda094d8a7956668bdc41ff8bbfd285
NIK717 <nikitaolenich2015@gmail.com>
2026-05-02 10:01:48 +0800

    My first commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameInventoryProject
+{
+    class Program
+    {
+        const int MaxValue = 100;
+        static string[] NameItem = new string[MaxValue];
+        static int[] WeightItem = new int[MaxValue];
+        static int[] CostItem = new int[MaxValue];
+        static int Count = 0;
+        
+
+        static void ShowsMenu()
+        {
+            Console.WriteLine("======== Система инвентаря ========");
+            Console.WriteLine("1. Просмотр всех предметов");
+            Console.WriteLine("2. Добавить предмет");
+            Console.WriteLine("3. Выход");
+            Console.WriteLine("Выберите действия");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Управление инвентарём";
+            bool isRunning = true;
+            while (isRunning)
+            {
+                Console.Clear();
+                ShowsMenu();
+                string choise = Console.ReadLine();
+
+                switch (choise)
+                {
+                    case "1":
+                        ViewItems();
+                        Console.ReadKey(); 
+                        break;
+                    case "2":
+                        AddItem();
+                        Console.ReadKey();
+                        break;
+                    case "3":
+                        isRunning = false;
+                        Console.Write("Программа завершена");
+                        break;
+                    default:
+                        Console.WriteLine("Вы ввели неправильную команду!");
+                        break;
+                }
+
+            }
+
+            
+
+        }
+
+        static void ViewItems()
+        {
+            Console.WriteLine("Список предметов");
+
+            if (Count == 0)
+            {
+                Console.WriteLine("Список предметов пуст");
+                return;
+            }
+
+            Console.WriteLine("{0,-10} {1, -10} {2, -10}", "Приедмет", "Вес(г)", "Стоимость");
+            Console.WriteLine(new string('-', 45));
+
+            for (int i = 0; i < Count; i++)
+            {
+                Console.WriteLine("{0,-10} {1, -10} {2, -10}",
+                    NameItem[i], WeightItem[i], CostItem[i]);
+            }
+
+            Console.WriteLine(new string('-', 45));
+            Console.WriteLine($"Количество записей {Count}");
+        }
+
+        static void AddItem()
+        {
+            Console.WriteLine("Добавить предмет");
+
+            if (Count >= MaxValue)
+            {
+                Console.WriteLine("Ошибка добавленияб, нет места");
+                return;
+            }
+
+
+            string newItemName = string.Empty;
+            while (true)
+            {
+                Console.WriteLine("Введите название предмета");
+                newItemName = Console.ReadLine();
+
+                bool isDuplicate = false;
+                for (int i = 0; i < Count; i++)
+                {
+                    if (NameItem[i] == newItemName)
+                    {
+                        isDuplicate = true;
+                        break;
+                    }
+
+                }
+
+                if (isDuplicate)
+                {
+                    Console.WriteLine("Данный предмет уже существует");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("Введите вес(г)");
+            int weightItem = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите стоимость");
+            int costItem = int.Parse(Console.ReadLine());
+
+            NameItem[Count] = newItemName;
+            WeightItem[Count] = weightItem;
+            CostItem[Count] = costItem;
+            Count++;
+            Console.WriteLine("Запись добавлена");
+        }
+        
+    }
+}
